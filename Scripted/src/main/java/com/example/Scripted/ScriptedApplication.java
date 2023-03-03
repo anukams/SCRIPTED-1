aae3178749b4649dd9bcaff95fb761ac6843deb9
Stanley <anukamstanley@gmail.com>
2023-03-03 16:42:04 -0500

    initial commit

---
+++ Scripted/src/main/java/com/example/Scripted/ScriptedApplication.java
@@ -0,0 +1,13 @@
+package com.example.Scripted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptedApplication.class, args);
+	}
+
+}
---
+++ Scripted/src/test/java/com/example/Scripted/ScriptedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Scripted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
